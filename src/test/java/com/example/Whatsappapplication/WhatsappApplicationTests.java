e4009625c1eaeeeb0a686fe55d31081d5f40d96a
Vereesh Hereemata <vereesh.hereemata@saswatfinance.com>
2024-05-29 12:54:03 +0530

    Again modified

--- src/test/java/com/example/Whatsappapplication/WhatsappApplicationTests.java
+++ src/test/java/com/example/Whatsappapplication/WhatsappApplicationTests.java
@@ -1,14 +1,14 @@
 package com.example.Whatsappapplication;
 
-import org.junit.Test;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 
 
 @SpringBootTest
-public class WhatsappApplicationTests {
+class WhatsappApplicationTests {
 
 
 
